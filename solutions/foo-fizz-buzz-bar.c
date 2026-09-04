72139f4b0e8b950a80f73ec529742a2a021a1955
isaiah0311 <isaiahl0311@gmail.com>
2026-09-03 17:02:29 -1000

    Improve solution for Foo Fizz Buzz Bar

--- solutions/foo-fizz-buzz-bar.c
+++ solutions/foo-fizz-buzz-bar.c
@@ -1,1 +1,2 @@
-main(i){for(i=0;++i<1001;){i%2?0:printf("Foo");i%3?0:printf("Fizz");i%5?0:printf("Buzz");i%7?0:printf("Bar");i%2&&i%3&&i%5&&i%7?printf("%d",i):0;puts("");}}
+#define P printf(
+main(i){for(i=0;++i<1001;){i%2?0:P"Foo");i%3?0:P"Fizz");i%5?0:P"Buzz");i%7?0:P"Bar");i%2&&i%3&&i%5&&i%7?P"%d",i):0;P"\n");}}
